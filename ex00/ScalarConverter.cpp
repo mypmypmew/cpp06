14845a5b8950ae48b1972a287051ba761f1d9632
Sergey Nazarov <lynchsama@Sergeys-MacBook-Pro.local>
2025-12-23 16:15:05 +0100

    float + double

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -5,6 +5,9 @@
 
 enum LiteralType {
     TYPE_CHAR,
+    TYPE_INT,
+    TYPE_DOUBLE,
+    TYPE_FLOAT,
     TYPE_PSEUDO_FLOAT,
     TYPE_PSEUDO_DOUBLE,
     TYPE_INVALID
@@ -13,6 +16,7 @@ enum LiteralType {
 static bool isPseudoDouble(const std::string& s);
 static bool isPseudoFloat(const std::string& s);
 static bool isCharLiteral(const std::string& s);
+static bool isIntLiteral(const std::string& s);
 static LiteralType detectType(const std::string& s);
 
 void ScalarConverter::convert(const std::string& s) {
@@ -32,6 +36,62 @@ static bool isCharLiteral(const std::string& s) {
     return (s.size() == 1 && !std::isdigit(static_cast<unsigned char>(s[0])));
 }
 
+static bool isIntLiteral(const std::string& s) {
+    std::size_t i = 0;
+    if (s.empty())
+        return false;
+    if (s[0] == '-' || s[0] == '+')
+        i++;
+    if (i == s.size())
+        return false;
+    for (; i < s.size(); i++) {
+        if (!std::isdigit(static_cast<unsigned char>(s[i]))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool isDoubleLiteral(const std::string& s) {
+    std::size_t i = 0;
+    int dotCount = 0;
+    bool hasDigit = false;
+
+    if (s.empty())
+        return false;
+    if (s[0] == '-' || s[0] == '+')
+        i++;
+    if (i == s.size())
+        return false;
+    for (; i < s.size(); i++) {
+        if (std::isdigit(static_cast<unsigned char>(s[i]))) {
+            hasDigit = true;
+        } else {
+            if (s[i] == '.') {
+                dotCount++;
+            } else {
+                return false;
+            }
+        }
+    }
+    if (dotCount == 1 && hasDigit)
+        return true;
+    else 
+        return false;
+}
+
+static bool isFloatLiteral(const std::string& s) {
+    if (s.size() < 2)
+        return false;
+    if (s[s.size() - 1] != 'f')
+        return false;
+    if (isIntLiteral(s.substr(0, s.size() - 1)))
+        return true;
+    if (isDoubleLiteral(s.substr(0, s.size() - 1)))
+        return true;
+    return false;
+}
+
 static LiteralType detectType(const std::string& s) {
     if (isPseudoDouble(s)) 
         return TYPE_PSEUDO_DOUBLE;
@@ -39,5 +99,11 @@ static LiteralType detectType(const std::string& s) {
         return TYPE_PSEUDO_FLOAT;
     if (isCharLiteral(s))  
         return TYPE_CHAR;
+    if (isIntLiteral(s))
+        return TYPE_INT; 
+    if (isDoubleLiteral(s))
+        return TYPE_DOUBLE;
+    if (isFloatLiteral(s))
+        return TYPE_FLOAT;   
     return TYPE_INVALID;
 }
