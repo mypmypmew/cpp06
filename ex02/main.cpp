e6ae10deb4bfa02ae2993ff5f49d627b17f65f5d
Serhii Nazarov <mypmypmew.dev@gmail.com>
2025-12-26 10:28:49 +0100

    identify function's

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,10 +1,10 @@
 #include "Base.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
 
 
 Base *generate() {
-
     int randomNum = rand() % 3;
     if(randomNum == 0)
         return new A;
@@ -14,11 +14,46 @@ Base *generate() {
     return new C;
 }
 
+void identify(Base& p) {
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A\n";
+        return;
+    } catch (...) {}
+
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B\n";
+        return;
+    } catch (...) {}
+
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C\n";
+        return;
+    } catch (...) {}
+
+    std::cout << "Unknown\n";
+}
+
+
+void identify(Base* p) {
+    if (dynamic_cast<A*>(p))
+        std::cout << "A\n";
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B\n";
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C\n";
+    else
+        std::cout << "Unknown\n";
+}
+
 int main() {
     std::srand(static_cast<unsigned>(std::time(NULL)));
 
     Base* p = generate();
-    std::cout << "generated pointer: " << p << "\n";
+    identify(p);
+    identify(*p);
     delete p;
 
     return 0;
